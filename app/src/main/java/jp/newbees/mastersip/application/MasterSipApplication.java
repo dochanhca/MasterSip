5bc78d99f2163d4555b62dad22089748735c51cf
ducpv <ducpv@nal.vn>
2016-12-08 17:51:55 +0700

    add fonts

---
+++ app/src/main/java/jp/newbees/mastersip/application/MasterSipApplication.java
@@ -0,0 +1,26 @@
+package jp.newbees.mastersip.application;
+
+import android.app.Application;
+
+import jp.newbees.mastersip.utils.FontUtils;
+
+/**
+ * Created by ducpv on 12/8/16.
+ */
+
+public class MasterSipApplication extends Application {
+
+    private static MasterSipApplication mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+
+        FontUtils.getInstance().initFonts(getApplicationContext());
+    }
+
+    public static synchronized MasterSipApplication getInstance() {
+        return mInstance;
+    }
+}
