9d4fa3bddac67694103fbc7cfcb437abcfdcce24
ducpv <ducpv@nal.vn>
2016-12-14 08:32:57 +0700

    update gitignore

---
+++ app/src/main/java/jp/newbees/mastersip/ui/auth/RegisterProfileFemaleActivity.java
@@ -0,0 +1,8 @@
+package jp.newbees.mastersip.ui.auth;
+
+/**
+ * Created by ducpv on 12/13/16.
+ */
+
+public class RegisterProfileFemaleActivity {
+}
--- app/src/main/java/jp/newbees/mastersip/ui/auth/RegisterProfileMaleActivity.java
+++ app/src/main/java/jp/newbees/mastersip/ui/auth/RegisterProfileMaleActivity.java
@@ -3,7 +3,6 @@ package jp.newbees.mastersip.ui.auth;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import jp.newbees.mastersip.R;
 import jp.newbees.mastersip.ui.BaseActivity;
@@ -13,21 +12,16 @@ import jp.newbees.mastersip.ui.top.TopActivity;
  * Created by vietbq on 12/6/16.
  */
 
-public class RegisterInfoActivity extends BaseActivity implements View.OnClickListener {
+public class RegisterProfileMaleActivity extends BaseActivity implements View.OnClickListener {
 
-    private Button btnRegisterInfo;
 
     @Override
     protected int layoutId() {
-        return R.layout.activity_register_info;
+        return R.layout.activity_register_profile_male;
     }
 
     @Override
     protected void initViews(Bundle savedInstanceState) {
-
-        btnRegisterInfo = (Button) findViewById(R.id.btn_register_info);
-
-        btnRegisterInfo.setOnClickListener(this);
 
         initHeader(getString(R.string.register_info_activity));
     }
