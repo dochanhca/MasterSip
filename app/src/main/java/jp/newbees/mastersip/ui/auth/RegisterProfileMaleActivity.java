8f4c2fc8f22303625644eb7359896bd884b0abc9
ducpv <ducpv@nal.vn>
2016-12-19 16:46:42 +0700

    handle case cancel crop image

--- app/src/main/java/jp/newbees/mastersip/ui/auth/RegisterProfileMaleActivity.java
+++ app/src/main/java/jp/newbees/mastersip/ui/auth/RegisterProfileMaleActivity.java
@@ -111,10 +111,6 @@ public class RegisterProfileMaleActivity extends BaseActivity implements View.On
             case SelectAvatarDialog.CROP_IMAGE:
                 if (resultCode == RESULT_OK) {
                     handleImageCropped(data);
-                } else if (resultCode == RESULT_CANCELED) {
-                    showAvatar();
-                    imgAvatar.setImageURI(null);
-                    imgAvatar.setImageURI(pickedImage);
                 }
                 break;
             case InputActivity.INPUT_ACTIVITY_REQUEST_CODE:
